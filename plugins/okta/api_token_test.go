7c9186c9612f3f47a2e56219c438df3469ecf0f8
Floris van der Grinten <floris.vandergrinten@agilebits.com>
2022-11-17 15:56:30 +0100

    Add Okta importer

---
+++ plugins/okta/api_token_test.go
@@ -0,0 +1,43 @@
+package okta
+
+import (
+	"testing"
+
+	"github.com/1Password/shell-plugins/sdk"
+	"github.com/1Password/shell-plugins/sdk/plugintest"
+	"github.com/1Password/shell-plugins/sdk/schema/fieldname"
+)
+
+func TestAPITokenImporter(t *testing.T) {
+	importer := APIToken().Importer
+
+	plugintest.TestImporter(t, importer, map[string]plugintest.ImportCase{
+		"environment": {
+			Environment: map[string]string{
+				"OKTA_CLIENT_TOKEN":  "dIzt9kbedfNLtBNvWaprp39MaffIVjWxkZBEXAMPLE",
+				"OKTA_CLIENT_ORGURL": "https://acme.okta.com",
+			},
+			ExpectedCandidates: []sdk.ImportCandidate{
+				{
+					Fields: map[string]string{
+						fieldname.Token: "dIzt9kbedfNLtBNvWaprp39MaffIVjWxkZBEXAMPLE",
+						FieldNameOrgURL: "https://acme.okta.com",
+					},
+				},
+			},
+		},
+		"Okta config file": {
+			Files: map[string]string{
+				"~/.okta/okta.yaml": plugintest.LoadFixture(t, "okta.yaml"),
+			},
+			ExpectedCandidates: []sdk.ImportCandidate{
+				{
+					Fields: map[string]string{
+						fieldname.Token: "dIzt9kbedfNLtBNvWaprp39MaffIVjWxkZBEXAMPLE",
+						FieldNameOrgURL: "https://acme.okta.com",
+					},
+				},
+			},
+		},
+	})
+}
