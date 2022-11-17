ac30fd03c277ef2c36330c6a2331b784fa0b44b3
Floris van der Grinten <floris.vandergrinten@agilebits.com>
2022-11-17 15:58:27 +0100

    Add Heroku importer

---
+++ plugins/heroku/api_key_test.go
@@ -0,0 +1,53 @@
+package heroku
+
+import (
+	"testing"
+
+	"github.com/1Password/shell-plugins/sdk"
+	"github.com/1Password/shell-plugins/sdk/plugintest"
+	"github.com/1Password/shell-plugins/sdk/schema/fieldname"
+)
+
+func TestAPIKeyImporter(t *testing.T) {
+	importer := APIKey().Importer
+
+	plugintest.TestImporter(t, importer, map[string]plugintest.ImportCase{
+		"environment": {
+			Environment: map[string]string{
+				"HEROKU_API_KEY": "dh7k7m662pqglxaybr1p0gpg1cu33example",
+			},
+			ExpectedCandidates: []sdk.ImportCandidate{
+				{
+					Fields: map[string]string{
+						fieldname.APIKey: "dh7k7m662pqglxaybr1p0gpg1cu33example",
+					},
+				},
+			},
+		},
+		"netrc file": {
+			Files: map[string]string{
+				"~/.netrc": plugintest.LoadFixture(t, "netrc"),
+			},
+			ExpectedCandidates: []sdk.ImportCandidate{
+				{
+					NameHint: "wendy@appleseed.com",
+					Fields: map[string]string{
+						fieldname.APIKey: "dh7k7m662pqglxaybr1p0gpg1cu33example",
+					},
+				},
+				{
+					NameHint: "wendy@appleseed.com",
+					Fields: map[string]string{
+						fieldname.APIKey: "dh7k7m662pqglxaybr1p0gpg1cu33example",
+					},
+				},
+			},
+		},
+		"netrc file non-Heroku": {
+			Files: map[string]string{
+				"~/.netrc": plugintest.LoadFixture(t, "netrc-non-heroku"),
+			},
+			ExpectedCandidates: []sdk.ImportCandidate{},
+		},
+	})
+}
