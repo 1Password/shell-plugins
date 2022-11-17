01c26366cc7d9837716974f0c41eb5042eea520f
Floris van der Grinten <floris.vandergrinten@agilebits.com>
2022-11-17 15:56:13 +0100

    Add import test helpers

---
+++ sdk/plugintest/importer_test_helper.go
@@ -0,0 +1,95 @@
+package plugintest
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"path/filepath"
+	"runtime"
+	"testing"
+
+	"github.com/1Password/shell-plugins/sdk"
+	"github.com/stretchr/testify/assert"
+)
+
+// TestImporter will run the importer for each specified case, mounting the specified files in a temp dir,
+// setting the environment variables, and configuring the home path using the temp dir.
+func TestImporter(t *testing.T, importer sdk.Importer, cases map[string]ImportCase) {
+	t.Helper()
+	for description, c := range cases {
+		if c.ExpectedOutput != nil && len(c.ExpectedCandidates) > 0 {
+			t.Fatal("ExpectedOutput and ExpectedCandidates can't both be set in the same test case")
+		}
+
+		for envVarName, value := range c.Environment {
+			t.Setenv(envVarName, value)
+		}
+
+		fsRoot := t.TempDir()
+		in := sdk.ImportInput{
+			HomeDir: filepath.Join(fsRoot, "~"),
+		}
+
+		for path, contents := range c.Files {
+			path = filepath.Join(fsRoot, path)
+			err := os.MkdirAll(filepath.Dir(path), 0700)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			err = os.WriteFile(path, []byte(contents), 0600)
+			if err != nil {
+				t.Fatal(err)
+			}
+		}
+
+		out := sdk.ImportOutput{}
+		importer(context.TODO(), in, &out)
+
+		description = fmt.Sprintf("Import: %s", description)
+		if c.ExpectedOutput != nil {
+			assert.Equal(t, *c.ExpectedOutput, out, description)
+		} else {
+			assert.ElementsMatch(t, c.ExpectedCandidates, out.AllCandidates(), description)
+		}
+
+		for envVarName := range c.Environment {
+			t.Setenv(envVarName, "")
+		}
+	}
+}
+
+// LoadFixture loads the test fixture file from the "test-fixtures" dir in the plugin directory.
+// It fails the test if the file can't be loaded.
+func LoadFixture(t *testing.T, filename string) string {
+	t.Helper()
+
+	_, testFilename, _, ok := runtime.Caller(1)
+	if !ok {
+		t.Fatal()
+	}
+
+	fixturePath := filepath.Join(filepath.Dir(testFilename), "test-fixtures", filename)
+	contents, err := os.ReadFile(fixturePath)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	return string(contents)
+}
+
+type ImportCase struct {
+	// Environment can be used to set environment variables for the importer test.
+	Environment map[string]string
+
+	// Files can be used to set files for the importer test, using the format: path -> contents.
+	// For example: ~/.config/my-pugin/config -> '{"foo":"bar"}'. This is useful in conjunction with the
+	// LoadFixture helper.
+	Files map[string]string
+
+	// ExpectedCandidates is a shorthand to set the expected import candidates. Mutually exclusive with ExpectedOutput.
+	ExpectedCandidates []sdk.ImportCandidate
+
+	// ExpectedOutput can be used to set the exact expected import output. Mutually exclusive with ExpectedCandidates.
+	ExpectedOutput *sdk.ImportOutput
+}
