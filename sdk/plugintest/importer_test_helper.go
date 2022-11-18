9123f019b87fd2be1b7ac9591cd411258e1d3962
Floris van der Grinten <floris.vandergrinten@agilebits.com>
2022-11-18 09:20:07 +0100

    Move test fixtures helper to separate file

--- sdk/plugintest/importer_test_helper.go
+++ sdk/plugintest/importer_test_helper.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
-	"runtime"
 	"testing"
 
 	"github.com/1Password/shell-plugins/sdk"
@@ -57,25 +56,6 @@ func TestImporter(t *testing.T, importer sdk.Importer, cases map[string]ImportCa
 			t.Setenv(envVarName, "")
 		}
 	}
-}
-
-// LoadFixture loads the test fixture file from the "test-fixtures" dir in the plugin directory.
-// It fails the test if the file can't be loaded.
-func LoadFixture(t *testing.T, filename string) string {
-	t.Helper()
-
-	_, testFilename, _, ok := runtime.Caller(1)
-	if !ok {
-		t.Fatal()
-	}
-
-	fixturePath := filepath.Join(filepath.Dir(testFilename), "test-fixtures", filename)
-	contents, err := os.ReadFile(fixturePath)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	return string(contents)
 }
 
 type ImportCase struct {
