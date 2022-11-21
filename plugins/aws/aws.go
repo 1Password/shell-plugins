582d0bea530125c0fe8552fbbb4399e8ca3bc376
volodymyrZotov <volodymyr.zotov@gmail.com>
2022-11-21 15:19:21 +0200

    Revert the change for the validate command failing test

--- plugins/aws/aws.go
+++ plugins/aws/aws.go
@@ -8,7 +8,7 @@ import (
 
 func AWSCLI() schema.Executable {
 	return schema.Executable{
-		//Runs:      []string{"aws"},
+		Runs:      []string{"aws"},
 		Name:      "AWS CLI",
 		DocsURL:   sdk.URL("https://aws.amazon.com/cli/"),
 		NeedsAuth: needsauth.NotForHelpOrVersion(),
