3835dd417dd5389727c5c26cab9afc3b4e30ad91
volodymyrZotov <volodymyr.zotov@gmail.com>
2022-11-22 14:48:16 +0200

    Remove redundant tests as they are covered in TestDatabaseCredentialsProvisioner

--- plugins/mysql/database_credentials_test.go
+++ plugins/mysql/database_credentials_test.go
@@ -1,74 +1,11 @@
 package mysql
 
 import (
-	"fmt"
 	"github.com/1Password/shell-plugins/sdk"
 	"github.com/1Password/shell-plugins/sdk/plugintest"
-	"github.com/1Password/shell-plugins/sdk/schema/fieldname"
 	"github.com/stretchr/testify/assert"
-	"strings"
 	"testing"
 )
-
-func TestMysqlConfigStartsWithClientGroup(t *testing.T) {
-	group := "[client]"
-	p := sdk.ProvisionInput{
-		ItemFields: map[string]string{
-			fieldname.User:     "root",
-			fieldname.Password: "root",
-		},
-	}
-	config, _ := mysqlConfig(p)
-
-	assert.True(t, strings.HasPrefix(string(config), group), fmt.Sprintf("should start with \"%s\" group", group))
-}
-
-func TestMysqlConfigHasPopulatedValues(t *testing.T) {
-	p := sdk.ProvisionInput{
-		ItemFields: map[string]string{
-			"host":     "localhost",
-			"port":     "3306",
-			"user":     "root",
-			"password": "root",
-			"database": "db",
-		},
-	}
-
-	config, _ := mysqlConfig(p)
-	entries := strings.Split(string(config), "\n")
-
-	cases := map[string]struct {
-		entryKey string
-	}{
-		"has host value": {
-			entryKey: "host",
-		},
-		"has port value": {
-			entryKey: "port",
-		},
-		"has user value": {
-			entryKey: "user",
-		},
-		"has password value": {
-			entryKey: "password",
-		},
-		"has database value": {
-			entryKey: "database",
-		},
-	}
-
-	for name, tc := range cases {
-		t.Run(name, func(t *testing.T) {
-			hasPopulatedValue := false
-			for _, entry := range entries {
-				if strings.Contains(entry, tc.entryKey) {
-					hasPopulatedValue = true
-				}
-			}
-			assert.True(t, hasPopulatedValue)
-		})
-	}
-}
 
 func TestMysqlConfigHandleEmptyItemFields(t *testing.T) {
 	p := sdk.ProvisionInput{
