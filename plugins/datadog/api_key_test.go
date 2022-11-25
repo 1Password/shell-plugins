43a907be2f0298a50d435827d6e1f3c540c8df68
volodymyrZotov <volodymyr.zotov@gmail.com>
2022-11-25 18:18:11 +0200

    Add datadog tests

---
+++ plugins/datadog/api_key_test.go
@@ -0,0 +1,45 @@
+package datadog
+
+import (
+	"testing"
+
+	"github.com/1Password/shell-plugins/sdk"
+	"github.com/1Password/shell-plugins/sdk/plugintest"
+	"github.com/1Password/shell-plugins/sdk/schema/fieldname"
+)
+
+func TestAPIKeyImporter(t *testing.T) {
+	plugintest.TestImporter(t, APIKey().Importer, map[string]plugintest.ImportCase{
+		"environment": {
+			Environment: map[string]string{
+				"DATADOG_API_KEY": "vvt4tzq3td4cb3wklna5bclv0example",
+				"DATADOG_APP_KEY": "g3jesabd5z8mm61ax7a0k8rzgju1yeps4example",
+			},
+			ExpectedCandidates: []sdk.ImportCandidate{
+				{
+					Fields: map[string]string{
+						fieldname.APIKey: "vvt4tzq3td4cb3wklna5bclv0example",
+						fieldname.AppKey: "g3jesabd5z8mm61ax7a0k8rzgju1yeps4example",
+					},
+				},
+			},
+		},
+	})
+}
+
+func TestAPIKeyProvisioner(t *testing.T) {
+	plugintest.TestProvisioner(t, APIKey().Provisioner, map[string]plugintest.ProvisionCase{
+		"default": {
+			ItemFields: map[string]string{
+				fieldname.APIKey: "vvt4tzq3td4cb3wklna5bclv0example",
+				fieldname.AppKey: "g3jesabd5z8mm61ax7a0k8rzgju1yeps4example",
+			},
+			ExpectedOutput: sdk.ProvisionOutput{
+				Environment: map[string]string{
+					"DATADOG_API_KEY": "vvt4tzq3td4cb3wklna5bclv0example",
+					"DATADOG_APP_KEY": "g3jesabd5z8mm61ax7a0k8rzgju1yeps4example",
+				},
+			},
+		},
+	})
+}
