3cc8cc90cc806e481ab25ba727c7b5ab0d18b244
Floris van der Grinten <floris.vandergrinten@agilebits.com>
2022-12-09 10:05:09 +0100

    Revert "Make the executable prompt require input"

--- cmd/contrib/main.go
+++ cmd/contrib/main.go
@@ -100,9 +100,8 @@ func newPlugin() error {
 			Validate: survey.Required,
 		},
 		{
-			Name:     "Executable",
-			Prompt:   &survey.Input{Message: `Executable name (e.g. "aws" or "gh") [required]`},
-			Validate: survey.Required,
+			Name:   "Executable",
+			Prompt: &survey.Input{Message: `Executable name (e.g. "aws" or "gh")`},
 		},
 		{
 			Name: "CredentialName",
