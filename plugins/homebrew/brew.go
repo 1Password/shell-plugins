44c77bf5488897221bc85f27a777d036bc4e7b94
Mark Dorison <mark@chromatichq.com>
2022-12-14 10:57:25 -0500

    Updated to only run authentication on specified commands.

--- plugins/homebrew/brew.go
+++ plugins/homebrew/brew.go
@@ -7,12 +7,19 @@ import (
 	"github.com/1Password/shell-plugins/sdk/schema/credname"
 )
 
+var commands = [][]string{
+	{"search"},
+
+	{"bump-cask-pr"},
+	{"bump-formula-pr"},
+}
+
 func HomebrewCLI() schema.Executable {
 	return schema.Executable{
 		Name:      "Homebrew CLI",
 		Runs:      []string{"brew"},
 		DocsURL:   sdk.URL("https://brew.sh/"),
-		NeedsAuth: needsauth.NotForHelpOrVersion(),
+		NeedsAuth: needsauth.ForCommands(commands...),
 		Uses: []schema.CredentialUsage{
 			{
 				Name: credname.PersonalAccessToken,
