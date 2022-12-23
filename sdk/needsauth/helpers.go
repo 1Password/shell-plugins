656364b48fd90f2488809664a347dc4c18bfba0d
Floris van der Grinten <floris.vandergrinten@agilebits.com>
2022-12-23 12:27:12 +0100

    Improve needsauth helper functions

--- sdk/needsauth/helpers.go
+++ sdk/needsauth/helpers.go
@@ -1,9 +1,11 @@
 package needsauth
 
-import "github.com/1Password/shell-plugins/sdk"
+import (
+	"github.com/1Password/shell-plugins/sdk"
+)
 
-// For returns a NeedsAuthentication rule that iterates over other NeedsAuthentication rules
-// until there's one that opts out of the authentication requirement.
+// For returns a NeedsAuthentication that opts in to the authentication requirement, unless there's
+// one rule specified that opts out to the authentication requirement.
 func For(rules ...sdk.NeedsAuthentication) sdk.NeedsAuthentication {
 	return func(in sdk.NeedsAuthenticationInput) bool {
 		for _, rule := range rules {
@@ -15,22 +17,33 @@ func For(rules ...sdk.NeedsAuthentication) sdk.NeedsAuthentication {
 	}
 }
 
-// ForCommands returns a NeedsAuthentication rule to require authentication for
-// certain (sub)command, e.g. ["account"] or ["account", "list"], ["account", "delete"].
-func ForCommands(commands ...[]string) sdk.NeedsAuthentication {
+// OnlyFor returns a NeedsAuthentication rule that only opts in of the authentication requirement if there's
+// at least one rule specified that opts in to the authentication requirement.
+func OnlyFor(rules ...sdk.NeedsAuthentication) sdk.NeedsAuthentication {
 	return func(in sdk.NeedsAuthenticationInput) bool {
-		for _, command := range commands {
-			if len(command) > len(in.CommandArgs) {
-				continue
+		for _, rule := range rules {
+			if rule(in) {
+				return true
 			}
+		}
+		return false
+	}
+}
 
-			for i := range command {
-				if command[i] != in.CommandArgs[i] {
-					break
-				}
-				if i == len(command)-1 {
-					return true
-				}
+// ForCommand returns a NeedsAuthentication rule to require authentication for
+// certain (sub)command, e.g. ["account"] or ["account", "list"].
+func ForCommand(command ...string) sdk.NeedsAuthentication {
+	return func(in sdk.NeedsAuthenticationInput) bool {
+		if len(command) > len(in.CommandArgs) {
+			return false
+		}
+
+		for i := range command {
+			if command[i] != in.CommandArgs[i] {
+				return false
+			}
+			if i == len(command)-1 {
+				return true
 			}
 		}
 
@@ -45,15 +58,52 @@ func Always() sdk.NeedsAuthentication {
 	}
 }
 
-// NotForArgs returns a NeedsAuthentication rule to not require authentication when
-// certain command-line args or flags are present.
-func NotForArgs(argsToSkip ...string) sdk.NeedsAuthentication {
+// NotForExactArgs returns a NeedsAuthentication rule to opt out of authentication when
+// the command-line args are an exact match with the passed in args.
+func NotForExactArgs(argsToSkip ...string) sdk.NeedsAuthentication {
 	return func(in sdk.NeedsAuthenticationInput) bool {
-		for _, commandArg := range in.CommandArgs {
-			for _, ignoreArg := range argsToSkip {
-				if commandArg == ignoreArg {
-					return false
+		if len(in.CommandArgs) != len(argsToSkip) {
+			return true
+		}
+
+		for i, commandArg := range in.CommandArgs {
+			if commandArg != argsToSkip[i] {
+				return true
+			}
+		}
+
+		return false
+	}
+}
+
+// NotWhenContainsArgs returns a NeedsAuthentication rule to not require authentication when
+// the exact sequence of argsToSkip is present somewhere in the command-line args.
+func NotWhenContainsArgs(argsToSkip ...string) sdk.NeedsAuthentication {
+	return func(in sdk.NeedsAuthenticationInput) bool {
+		if len(argsToSkip) == 0 {
+			return true
+		}
+
+		if len(argsToSkip) > len(in.CommandArgs) {
+			return true
+		}
+
+		for i := range in.CommandArgs {
+			if i+len(argsToSkip) > len(in.CommandArgs) {
+				return true
+			}
+
+			matches := true
+			for i, argsToCompare := range in.CommandArgs[i : i+len(argsToSkip)] {
+				if argsToCompare != argsToSkip[i] {
+					matches = false
 				}
+			}
+
+			// If the argsToSkip are found in the command-line args, return that the command
+			// does not not require authentication
+			if matches {
+				return false
 			}
 		}
 		return true
