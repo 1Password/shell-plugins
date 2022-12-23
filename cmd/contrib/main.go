e95d02aa586743e35892626136831226410d178d
Floris van der Grinten <floris.vandergrinten@agilebits.com>
2022-12-23 12:30:30 +0100

    Include new needsauth helper in template

--- cmd/contrib/main.go
+++ cmd/contrib/main.go
@@ -518,7 +518,10 @@ func {{ .PlatformNameUpperCamelCase }}CLI() schema.Executable {
 		Name:      "{{ .PlatformName }} CLI", // TODO: Check if this is correct
 		Runs:      []string{"{{ .Executable }}"},
 		DocsURL:   sdk.URL("https://{{ .Name }}.com/docs/cli"), // TODO: Replace with actual URL
-		NeedsAuth: needsauth.NotForHelpOrVersion(),
+		NeedsAuth: needsauth.For(
+			needsauth.NotForHelpOrVersion(),
+			needsauth.NotWithoutArgs(),
+		),
 		{{- if .CredentialName }}
 		Uses: []schema.CredentialUsage{
 			{
