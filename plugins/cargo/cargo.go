cc4e8b0232d878237db2ee747a6cc86b806adbb7
Amanda Crawley <amanda.crawley@agilebits.com>
2023-01-10 15:02:46 -0400

    auth required only for publish, owner and yank

--- plugins/cargo/cargo.go
+++ plugins/cargo/cargo.go
@@ -7,12 +7,18 @@ import (
 	"github.com/1Password/shell-plugins/sdk/schema/credname"
 )
 
+var commands = [][]string{
+	{"publish"},
+	{"yank"},
+	{"owner"},
+}
+
 func CargoCLI() schema.Executable {
 	return schema.Executable{
 		Name:      "Cargo CLI",
 		Runs:      []string{"cargo"},
 		DocsURL:   sdk.URL("https://doc.rust-lang.org/cargo/index.html"),
-		NeedsAuth: needsauth.NotForHelpOrVersion(),
+		NeedsAuth: needsauth.ForCommands(commands...),
 		Uses: []schema.CredentialUsage{
 			{
 				Name: credname.APIToken,
