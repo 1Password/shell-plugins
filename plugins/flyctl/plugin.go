8097d41cfa13d9f07ab9b207bc9380fa3ee66edf
Arun <arun@hey.com>
2023-01-12 02:08:13 -0500

    Add support for flyctl

---
+++ plugins/flyctl/plugin.go
@@ -0,0 +1,22 @@
+package flyctl
+
+import (
+	"github.com/1Password/shell-plugins/sdk"
+	"github.com/1Password/shell-plugins/sdk/schema"
+)
+
+func New() schema.Plugin {
+	return schema.Plugin{
+		Name: "flyctl",
+		Platform: schema.PlatformInfo{
+			Name:     "Fly.io",
+			Homepage: sdk.URL("https://fly.io/docs/flyctl/"),
+		},
+		Credentials: []schema.CredentialType{
+			AccessToken(),
+		},
+		Executables: []schema.Executable{
+			FlyctlCLI(),
+		},
+	}
+}
