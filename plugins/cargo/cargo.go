c64675e6b99168c307bb011a28d0d6aa5020f75b
Horia Culea <horia.culea@agilebits.com>
2023-01-18 14:16:53 +0200

    Bring all commands to latest needsauth protocol version

--- plugins/cargo/cargo.go
+++ plugins/cargo/cargo.go
@@ -7,18 +7,16 @@ import (
 	"github.com/1Password/shell-plugins/sdk/schema/credname"
 )
 
-var commands = [][]string{
-	{"publish"},
-	{"yank"},
-	{"owner"},
-}
-
 func CargoCLI() schema.Executable {
 	return schema.Executable{
-		Name:      "Cargo CLI",
-		Runs:      []string{"cargo"},
-		DocsURL:   sdk.URL("https://doc.rust-lang.org/cargo/index.html"),
-		NeedsAuth: needsauth.ForCommands(commands...),
+		Name:    "Cargo CLI",
+		Runs:    []string{"cargo"},
+		DocsURL: sdk.URL("https://doc.rust-lang.org/cargo/index.html"),
+		NeedsAuth: needsauth.OnlyFor(
+			needsauth.ForCommand("publish"),
+			needsauth.ForCommand("yank"),
+			needsauth.ForCommand("owner"),
+		),
 		Uses: []schema.CredentialUsage{
 			{
 				Name: credname.APIToken,
