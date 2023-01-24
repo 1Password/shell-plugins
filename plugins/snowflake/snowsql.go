a4ca38bd2347ffa89c80f348a301eb4120f46771
William Park <william.park@agilebits.com>
2023-01-24 15:15:34 -0500

    Specify arguments that result in 1Password auth opt out

--- plugins/snowflake/snowsql.go
+++ plugins/snowflake/snowsql.go
@@ -9,10 +9,20 @@ import (
 
 func SnowflakeCLI() schema.Executable {
 	return schema.Executable{
-		Name:      "Snowflake",
-		Runs:      []string{"snowsql"},
-		DocsURL:   sdk.URL("https://docs.snowflake.com/en/user-guide/snowsql.html"),
-		NeedsAuth: needsauth.NotForHelpOrVersion(),
+		Name:    "Snowflake",
+		Runs:    []string{"snowsql"},
+		DocsURL: sdk.URL("https://docs.snowflake.com/en/user-guide/snowsql.html"),
+		NeedsAuth: needsauth.IfAll(
+			needsauth.NotForHelpOrVersion(),
+			needsauth.NotWhenContainsArgs("-a"),
+			needsauth.NotWhenContainsArgs("--accountname"),
+			needsauth.NotWhenContainsArgs("-u"),
+			needsauth.NotWhenContainsArgs("--username"),
+			needsauth.NotWhenContainsArgs("--authenticator"),
+			needsauth.NotWhenContainsArgs("--config"),
+			needsauth.NotWhenContainsArgs("-P"),
+			needsauth.NotWhenContainsArgs("--prompt"),
+		),
 		Uses: []schema.CredentialUsage{
 			{
 				Name: credname.Credentials,
