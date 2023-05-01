87b81b920ea78d5ec0b7b1ed9603b51590ba0cec
Andi Titu <andi.titu@agilebits.com>
2023-05-01 15:03:47 +0200

    remove some validation checks

--- sdk/schema/plugin.go
+++ sdk/schema/plugin.go
@@ -67,14 +67,8 @@ func (p Plugin) Validate() (bool, ValidationReport) {
 	})
 
 	report.AddCheck(ValidationCheck{
-		Description: "Has a credential type or executable defined",
-		Assertion:   len(p.Credentials) > 0 && len(p.Executables) > 0,
-		Severity:    ValidationSeverityError,
-	})
-
-	report.AddCheck(ValidationCheck{
-		Description: "Has no more than one credential type defined. Plugins with multiple credential types are not supported yet",
-		Assertion:   len(p.Credentials) == 1,
+		Description: "Has an executable defined",
+		Assertion:   len(p.Executables) > 0,
 		Severity:    ValidationSeverityError,
 	})
 
