e548ff65d4a81fe997024df201ea6bae15b4dffc
Arun <arun@arun.blog>
2023-06-08 18:42:57 -0400

    Introduce a new arguments provisioner and a new chained provision that stitches together multiple provisioners

--- sdk/provisioner.go
+++ sdk/provisioner.go
@@ -102,9 +102,13 @@ func (out *ProvisionOutput) AddEnvVar(name string, value string) {
 	out.Environment[name] = value
 }
 
-// AddArgs can be used to add additional arguments to the command line of the provision output.
-func (out *ProvisionOutput) AddArgs(args ...string) {
-	out.CommandLine = append(out.CommandLine, args...)
+// AddArgs can be used to add additional arguments to the command line of the provision output, at a specific index.
+func (out *ProvisionOutput) AddArgsAtIndex(index uint, args ...string) {
+	newCommandLine := []string{}
+	newCommandLine = append(newCommandLine, out.CommandLine[:index]...)
+	newCommandLine = append(newCommandLine, args...)
+	newCommandLine = append(newCommandLine, out.CommandLine[index:]...)
+	out.CommandLine = newCommandLine
 }
 
 // AddSecretFile can be used to add a file containing secrets to the provision output.
