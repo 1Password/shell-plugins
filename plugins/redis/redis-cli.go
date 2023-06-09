3ab20bb61c63368d92fac21a54d971c92481b204
Arun <arun@arun.blog>
2023-06-09 15:16:53 -0400

    Remove Arguments Provisioner completely and introduce redis-specific arguments provision that injects arguments at the first index. This works along with the environment variable provisioner as a chained provision. This is a temporary arrangement until the Arguments Provision is reintroduced at the SDK-level.

--- plugins/redis/redis-cli.go
+++ plugins/redis/redis-cli.go
@@ -25,7 +25,7 @@ func RedisCLI() schema.Executable {
 				Name: credname.UserCredentials,
 				Provisioner: provision.ChainProvisioners(
 					provision.EnvVars(defaultEnvVarMapping),
-					provision.ArgsAtIndex(indexToProvisionAt, argsToProvision),
+					redisFlags(indexToProvisionAt, argsToProvision),
 				),
 			},
 		},
