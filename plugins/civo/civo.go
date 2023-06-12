6a5678b9523e6999ec13ebecdd34a708bac43a7e
Cheithanya <cheithanya2002@gmail.com>
2023-06-12 18:06:37 +0530

    civo plugin added

---
+++ plugins/civo/civo.go
@@ -0,0 +1,25 @@
+package civo
+
+import (
+	"github.com/1Password/shell-plugins/sdk"
+	"github.com/1Password/shell-plugins/sdk/needsauth"
+	"github.com/1Password/shell-plugins/sdk/schema"
+	"github.com/1Password/shell-plugins/sdk/schema/credname"
+)
+
+func CivoCLI() schema.Executable {
+	return schema.Executable{
+		Name:      "Civo CLI", // TODO: Check if this is correct
+		Runs:      []string{"civo"},
+		DocsURL:   sdk.URL("https://civo.com/docs/cli"), // TODO: Replace with actual URL
+		NeedsAuth: needsauth.IfAll(
+			needsauth.NotForHelpOrVersion(),
+			needsauth.NotWithoutArgs(),
+		),
+		Uses: []schema.CredentialUsage{
+			{
+				Name: credname.APIKey,
+			},
+		},
+	}
+}
