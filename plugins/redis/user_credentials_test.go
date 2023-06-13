336bcbbb2dcaf7199b16c61d768b327ffa98817b
Arun <arun@arun.blog>
2023-06-13 00:31:12 -0400

    Update test to verify the provisioned arguments and expected command line arguments but it's not working just yet due to slice range issues

--- plugins/redis/user_credentials_test.go
+++ plugins/redis/user_credentials_test.go
@@ -8,7 +8,25 @@ import (
 	"github.com/1Password/shell-plugins/sdk/schema/fieldname"
 )
 
-func TestPasswordProvisioner(t *testing.T) {
+func TestUserCredentialsProvisioner(t *testing.T) {
+	plugintest.TestProvisioner(t, redisProvisioner(), map[string]plugintest.ProvisionCase{
+		"default": {
+			ItemFields: map[sdk.FieldName]string{
+				fieldname.Password: "pjtxpc2gaddifapjvalggspojexample",
+				fieldname.Username: "example",
+				fieldname.Host:     "127.0.0.1",
+				fieldname.Port:     "6379",
+			},
+			ExpectedOutput: sdk.ProvisionOutput{
+				Environment: map[string]string{
+					"REDISCLI_AUTH": "pjtxpc2gaddifapjvalggspojexample",
+				},
+				CommandLine: []string{"-p", "6379", "-h", "127.0.0.1", "--user", "example"},
+			},
+		},
+	})
+}
+func TestDefaultUserCredentialsProvisioner(t *testing.T) {
 	plugintest.TestProvisioner(t, UserCredentials().DefaultProvisioner, map[string]plugintest.ProvisionCase{
 		"default": {
 			ItemFields: map[sdk.FieldName]string{
@@ -23,7 +41,7 @@ func TestPasswordProvisioner(t *testing.T) {
 	})
 }
 
-func TestPasswordImporter(t *testing.T) {
+func TestUserCredentialsImporter(t *testing.T) {
 	plugintest.TestImporter(t, UserCredentials().Importer, map[string]plugintest.ImportCase{
 		"environment": {
 			Environment: map[string]string{
