81b2ad3ddc2f8950492c93b5d0971db2b730c4c0
Cheithanya <cheithanya2002@gmail.com>
2023-06-18 11:33:57 +0530

    temp file provisioning

--- plugins/civo/api_key.go
+++ plugins/civo/api_key.go
@@ -3,6 +3,8 @@ package civo
 import (
 	"context"
 	"encoding/json"
+	"io/ioutil"
+	"os"
 
 	"github.com/1Password/shell-plugins/sdk"
 	"github.com/1Password/shell-plugins/sdk/importer"
@@ -10,6 +12,7 @@ import (
 	"github.com/1Password/shell-plugins/sdk/schema"
 	"github.com/1Password/shell-plugins/sdk/schema/credname"
 	"github.com/1Password/shell-plugins/sdk/schema/fieldname"
+	"github.com/mitchellh/go-homedir"
 )
 
 func APIKey() schema.CredentialType {
@@ -41,12 +44,113 @@ func APIKey() schema.CredentialType {
 				Optional:            true,
 			},
 		},
-		DefaultProvisioner: provision.EnvVars(defaultEnvVarMapping),
+		// DefaultProvisioner: provision.EnvVars(defaultEnvVarMapping),
+		DefaultProvisioner: provision.TempFile(configFile,
+			provision.Filename(".civo.json"),
+			provision.AddArgs(
+				"--config", "{{.Path}}",
+			),
+			provision.AtFixedPath("~/.civo.json"),
+		),
 		Importer: importer.TryAll(
 			importer.TryEnvVarPair(defaultEnvVarMapping),
 			TryCivoConfigFile(),
 		)}
 }
+func configFile(in sdk.ProvisionInput) ([]byte, error) {
+	apiKey := in.ItemFields[fieldname.APIKey]
+	apiKeyID := in.ItemFields[fieldname.APIKeyID]
+	defaultRegion := in.ItemFields[fieldname.DefaultRegion]
+
+	// Check if the file already exists
+	filePath := "~/.civo.json"
+	exists, err := fileExists(filePath)
+	if err != nil {
+		return nil, err
+	}
+
+	if exists {
+		// Read the existing file
+		contents, err := ioutil.ReadFile(filePath)
+		if err != nil {
+			return nil, err
+		}
+
+		var config Config
+		if err := json.Unmarshal(contents, &config); err != nil {
+			return nil, err
+		}
+
+		// Update the config with the new values
+		config.Properties[apiKeyID] = json.RawMessage(`"` + apiKey + `"`)
+		config.Meta.CurrentAPIKey = apiKeyID
+		config.Meta.DefaultRegion = defaultRegion
+
+		return json.MarshalIndent(config, "", "  ")
+	}
+
+	// Create a new config
+	config := Config{
+		Properties: map[string]json.RawMessage{
+			apiKeyID: json.RawMessage(`"` + apiKey + `"`),
+		},
+		Meta: struct {
+			Admin               bool   `json:"admin"`
+			CurrentAPIKey       string `json:"current_apikey"`
+			DefaultRegion       string `json:"default_region"`
+			LatestReleaseCheck  string `json:"latest_release_check"`
+			URL                 string `json:"url"`
+			LastCommandExecuted string `json:"last_command_executed"`
+		}{
+			CurrentAPIKey: apiKeyID,
+			DefaultRegion: defaultRegion,
+		},
+	}
+
+	return json.MarshalIndent(config, "", "  ")
+}
+
+func fileExists(filePath string) (bool, error) {
+	expandedPath, err := homedir.Expand(filePath)
+	if err != nil {
+		return false, err
+	}
+
+	info, err := os.Stat(expandedPath)
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	if err != nil {
+		return false, err
+	}
+
+	return !info.IsDir(), nil
+}
+
+// func configFile(in sdk.ProvisionInput) ([]byte, error) {
+// 	apiKey := in.ItemFields[fieldname.APIKey]
+// 	apiKeyID := in.ItemFields[fieldname.APIKeyID]
+// 	defaultRegion := in.ItemFields[fieldname.DefaultRegion]
+
+// 	config := Config{
+// 		Properties: map[string]json.RawMessage{
+// 			apiKeyID: json.RawMessage(`"` + apiKey + `"`),
+// 		},
+// 		Meta: struct {
+// 			Admin               bool   `json:"admin"`
+// 			CurrentAPIKey       string `json:"current_apikey"`
+// 			DefaultRegion       string `json:"default_region"`
+// 			LatestReleaseCheck  string `json:"latest_release_check"`
+// 			URL                 string `json:"url"`
+// 			LastCommandExecuted string `json:"last_command_executed"`
+// 		}{
+// 			CurrentAPIKey: apiKeyID,
+// 			DefaultRegion: defaultRegion,
+// 		},
+// 	}
+
+// 	return json.MarshalIndent(config, "", "  ")
+// }
 
 var defaultEnvVarMapping = map[string]sdk.FieldName{
 	"CIVO_API_KEY_NAME": fieldname.APIKeyID,
@@ -62,6 +166,7 @@ func TryCivoConfigFile() sdk.Importer {
 			return
 
 		}
+
 		if len(config.Properties) == 0 && config.Meta.CurrentAPIKey == "" {
 			return
 		}
