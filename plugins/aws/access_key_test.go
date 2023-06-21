42c6cfdf987754a2891dd6d55a99124d4f667aec
Andi Titu <andi.titu@agilebits.com>
2023-06-21 13:46:55 +0200

    Fix test

--- plugins/aws/access_key_test.go
+++ plugins/aws/access_key_test.go
@@ -620,7 +620,7 @@ func TestResolveLocalAnd1PasswordConfigurations(t *testing.T) {
 				ProfileName: "dev",
 				MfaSerial:   "arn:aws:iam::123456789012:mfa/user",
 			},
-			err: fmt.Errorf("MFA failed: an MFA serial was found but no OTP has been set up in 1Password"),
+			err: fmt.Errorf("MFA failed: the selected profile requires an OTP because an MFA serial (arn:aws:iam::123456789012:mfa/user) was detected but no 'One-Time Password' field was found in the associated item"),
 		},
 		{
 			description: "has region only in 1Password",
