3671af33d5fe648f88aac5c9efc9bb189e79e3ac
Smyja <akposlive59@gmail.com>
2023-06-22 17:05:44 +0100

    updated file struct
    Signed-off-by: Smyja <akposlive59@gmail.com>

--- plugins/render/api_key.go
+++ plugins/render/api_key.go
@@ -11,18 +11,29 @@ import (
 	"github.com/1Password/shell-plugins/sdk/schema/fieldname"
 )
 
+type Config struct {
+	Version           int
+	SSHPreserveHosts  bool `yaml:"sshPreserveHosts"`
+	Profiles          map[string]Profile
+}
+
+type Profile struct {
+	DefaultRegion string `yaml:"defaultRegion"`
+	APIKey        string `yaml:"apiKey"`
+}
+
 func APIKey() schema.CredentialType {
 	return schema.CredentialType{
 		Name:          credname.APIKey,
-		DocsURL:       sdk.URL("https://render.com/docs/api_key"), // TODO: Replace with actual URL
-		ManagementURL: sdk.URL("https://console.render.com/user/security/tokens"), // TODO: Replace with actual URL
+		DocsURL:       sdk.URL("https://render.com/docs/api_key"),
+		ManagementURL: sdk.URL("https://console.render.com/user/security/tokens"),
 		Fields: []schema.CredentialField{
 			{
 				Name:                fieldname.APIKey,
 				MarkdownDescription: "API Key used to authenticate to Render.",
 				Secret:              true,
 				Composition: &schema.ValueComposition{
-					Length: 10,
+					Length: 30,
 					Charset: schema.Charset{
 						Uppercase: true,
 						Lowercase: true,
@@ -35,36 +46,31 @@ func APIKey() schema.CredentialType {
 		Importer: importer.TryAll(
 			importer.TryEnvVarPair(defaultEnvVarMapping),
 			TryRenderConfigFile(),
-		)}
+		),
+	}
 }
 
 var defaultEnvVarMapping = map[string]sdk.FieldName{
-	"RENDER_API_KEY": fieldname.APIKey, // TODO: Check if this is correct
+	"RENDER_API_KEY": fieldname.APIKey,
 }
 
-// TODO: Check if the platform stores the API Key in a local config file, and if so,
-// implement the function below to add support for importing it.
 func TryRenderConfigFile() sdk.Importer {
-	return importer.TryFile("~/path/to/config/file.yml", func(ctx context.Context, contents importer.FileContents, in sdk.ImportInput, out *sdk.ImportAttempt) {
-		// var config Config
-		// if err := contents.ToYAML(&config); err != nil {
-		// 	out.AddError(err)
-		// 	return
-		// }
+	return importer.TryFile(".render/config.yaml", func(ctx context.Context, contents importer.FileContents, in sdk.ImportInput, out *sdk.ImportAttempt) {
+		var config Config
+		if err := contents.ToYAML(&config); err != nil {
+			out.AddError(err)
+			return
+		}
 
-		// if config.APIKey == "" {
-		// 	return
-		// }
+		profile, ok := config.Profiles["default"]
+		if !ok || profile.APIKey == "" {
+			return
+		}
 
-		// out.AddCandidate(sdk.ImportCandidate{
-		// 	Fields: map[sdk.FieldName]string{
-		// 		fieldname.APIKey: config.APIKey,
-		// 	},
-		// })
+		out.AddCandidate(sdk.ImportCandidate{
+			Fields: map[sdk.FieldName]string{
+				fieldname.APIKey: profile.APIKey,
+			},
+		})
 	})
 }
-
-// TODO: Implement the config file schema
-// type Config struct {
-//	APIKey string
-// }
