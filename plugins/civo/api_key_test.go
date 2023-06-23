97295a99742a42c25f40712e229443a2501cd583
siddhikhapare <siddhikhapare77@gmail.com>
2023-06-24 02:53:03 +0530

    civo plugin added

---
+++ plugins/civo/api_key_test.go
@@ -0,0 +1,54 @@
+package civo
+
+import (
+	"testing"
+	
+	"github.com/1Password/shell-plugins/sdk"
+	"github.com/1Password/shell-plugins/sdk/plugintest"
+	"github.com/1Password/shell-plugins/sdk/schema/fieldname"
+)
+	
+func TestAPIKeyProvisioner(t *testing.T) {
+	plugintest.TestProvisioner(t, APIKey().DefaultProvisioner, map[string]plugintest.ProvisionCase{
+		"default": {
+			ItemFields: map[sdk.FieldName]string{ 
+				fieldname.APIKey: "vvUmolh6TG7VMzCkhfWvOCJfpZMzM6W5hWkjurT63FoEXAMPLE",
+			},
+			ExpectedOutput: sdk.ProvisionOutput{
+				Environment: map[string]string{
+					"CIVO_TOKEN": "vvUmolh6TG7VMzCkhfWvOCJfpZMzM6W5hWkjurT63FoEXAMPLE",
+				},
+			},
+		},
+	})
+}
+
+func TestAPIKeyImporter(t *testing.T) {
+	plugintest.TestImporter(t, APIKey().Importer, map[string]plugintest.ImportCase{
+		"environment": {
+			Environment: map[string]string{ 
+				"CIVO_TOKEN": "vvUmolh6TG7VMzCkhfWvOCJfpZMzM6W5hWkjurT63FoEXAMPLE",
+			},
+			ExpectedCandidates: []sdk.ImportCandidate{
+				{
+					Fields: map[sdk.FieldName]string{
+						fieldname.APIKey: "vvUmolh6TG7VMzCkhfWvOCJfpZMzM6W5hWkjurT63FoEXAMPLE",
+					},
+				},
+			},
+		},
+		
+		"config file": {
+			Files: map[string]string{
+				"~/.civo.json": plugintest.LoadFixture(t, "civo.json"),
+			},
+			ExpectedCandidates: []sdk.ImportCandidate{
+				{
+					Fields: map[sdk.FieldName]string{
+						fieldname.Token: "vvUmolh6TG7VMzCkhfWvOCJfpZMzM6W5hWkjurT63FoEXAMPLE",
+					},
+				},
+			},
+		},
+	})
+}
