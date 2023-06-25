15c72ef89f8c71ebc51f171e025b1bb3b1c38157
Smyja <akposlive59@gmail.com>
2023-06-25 20:17:26 +0100

    Deta.space initital setup

---
+++ plugins/deta/plugin.go
@@ -0,0 +1,22 @@
+package deta
+
+import (
+	"github.com/1Password/shell-plugins/sdk"
+	"github.com/1Password/shell-plugins/sdk/schema"
+)
+
+func New() schema.Plugin {
+	return schema.Plugin{
+		Name: "deta",
+		Platform: schema.PlatformInfo{
+			Name:     "Deta",
+			Homepage: sdk.URL("https://deta.com"), // TODO: Check if this is correct
+		},
+		Credentials: []schema.CredentialType{
+			APIKey(),
+		},
+		Executables: []schema.Executable{
+			DetaCLI(),
+		},
+	}
+}
