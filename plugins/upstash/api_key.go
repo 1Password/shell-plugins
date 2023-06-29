d39a0fc3075b787a33c8fa957efa9e0a5ae1a192
siddhikhapare <siddhikhapare77@gmail.com>
2023-06-29 15:48:55 +0530

    changes added in files
    Signed-off-by: Amanda Crawley <amanda.crawley@agilebits.com>

---
+++ plugins/upstash/api_key.go
@@ -0,0 +1,81 @@
+package upstash
+
+import (
+	"context"
+
+	"github.com/1Password/shell-plugins/sdk"
+	"github.com/1Password/shell-plugins/sdk/importer"
+	"github.com/1Password/shell-plugins/sdk/provision"
+	"github.com/1Password/shell-plugins/sdk/schema"
+	"github.com/1Password/shell-plugins/sdk/schema/credname"
+	"github.com/1Password/shell-plugins/sdk/schema/fieldname"
+)
+
+func APIKey() schema.CredentialType {
+	return schema.CredentialType{
+		Name:          credname.APIKey,
+		DocsURL:       sdk.URL("https://docs.upstash.com/redis/account/developerapi#create-an-api-key"), // TODO: Replace with actual URL
+		ManagementURL: sdk.URL("https://console.upstash.com/account/api"), 
+		Fields: []schema.CredentialField{
+			{
+				Name:                fieldname.APIKey,
+				MarkdownDescription: "API Key used to authenticate to Upstash.",
+				Secret:              true,
+				Composition: &schema.ValueComposition{
+					Length: 36,
+					Charset: schema.Charset{
+						Lowercase: true,
+						Digits:    true,
+					},
+				},
+			},
+			{
+				Name:                fieldname.Email,
+				MarkdownDescription: "Email used to authenticate to Upstash.",
+				Composition: &schema.ValueComposition{
+					Charset: schema.Charset{
+						Uppercase: true,
+						Lowercase: true,
+						Digits:    true,
+						Symbols:   true,
+					},
+				},
+			},
+		},
+        DefaultProvisioner: provision.EnvVars(defaultEnvVarMapping),
+		Importer: importer.TryAll(
+			importer.TryEnvVarPair(defaultEnvVarMapping),
+			TryUpstashConfigFile(),
+		)}
+}
+
+var defaultEnvVarMapping = map[string]sdk.FieldName{
+	"UPSTASH_API_KEY": fieldname.APIKey, 
+	"UPSTASH_EMAIL" : fieldname.Email,
+}
+
+func TryUpstashConfigFile() sdk.Importer {
+	return importer.TryFile("~/.upstash.json", func(ctx context.Context, contents importer.FileContents, in sdk.ImportInput, out *sdk.ImportAttempt) {
+		var config Config
+		if err := contents.ToYAML(&config); err != nil {
+			out.AddError(err)
+			return
+		}
+
+		if config.APIKey == "" && config.Email == "" {
+			return
+		}
+
+		out.AddCandidate(sdk.ImportCandidate{
+			Fields: map[sdk.FieldName]string{
+				fieldname.APIKey: config.APIKey,
+				fieldname.Email : config.Email,
+			},
+		})
+	})
+}
+
+type Config struct {
+	APIKey string  `json:"apiKey"`
+	Email  string  `json:"email"`
+}
---
+++ plugins/upstash/upstash.go
@@ -0,0 +1,27 @@
+package upstash
+
+import (
+	"github.com/1Password/shell-plugins/sdk"
+	"github.com/1Password/shell-plugins/sdk/needsauth"
+	"github.com/1Password/shell-plugins/sdk/schema"
+	"github.com/1Password/shell-plugins/sdk/schema/credname"
+)
+
+func UpstashCLI() schema.Executable {
+	return schema.Executable{
+		Name:      "Upstash CLI", 
+		Runs:      []string{"upstash"},
+		DocsURL:   sdk.URL("https://github.com/upstash/cli"), 
+		NeedsAuth: needsauth.IfAll(
+			needsauth.NotForHelpOrVersion(),
+			needsauth.NotWithoutArgs(),
+			needsauth.NotForExactArgs("config"),
+			needsauth.NotWhenContainsArgs("auth"),
+		),
+		Uses: []schema.CredentialUsage{
+			{
+				Name: credname.APIKey,
+			},
+		},
+	}
+}
