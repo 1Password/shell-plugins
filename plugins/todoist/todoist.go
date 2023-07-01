43459c2621042c99cf6200bf7bde3e7fa41c6417
Maniraja <rajapri28613@gmail.com>
2023-06-30 20:12:54 -0400

    Add support for Todoist CLI

---
+++ plugins/todoist/todoist.go
@@ -0,0 +1,25 @@
+package todoist
+
+import (
+	"github.com/1Password/shell-plugins/sdk"
+	"github.com/1Password/shell-plugins/sdk/needsauth"
+	"github.com/1Password/shell-plugins/sdk/schema"
+	"github.com/1Password/shell-plugins/sdk/schema/credname"
+)
+
+func TodoistCLI() schema.Executable {
+	return schema.Executable{
+		Name:    "Todoist CLI",
+		Runs:    []string{"todoist"},
+		DocsURL: sdk.URL("https://github.com/sachaos/todoist"),
+		NeedsAuth: needsauth.IfAll(
+			needsauth.NotForHelpOrVersion(),
+			needsauth.NotWithoutArgs(),
+		),
+		Uses: []schema.CredentialUsage{
+			{
+				Name: credname.APIToken,
+			},
+		},
+	}
+}
