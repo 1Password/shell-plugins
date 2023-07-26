7343e00af7487e3474bca509560f1a746a958d50
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2023-07-26 09:37:07 -0400

    remove api key length, add management url

--- plugins/crowdin/access_token.go
+++ plugins/crowdin/access_token.go
@@ -11,15 +11,15 @@ import (
 
 func AccessToken() schema.CredentialType {
 	return schema.CredentialType{
-		Name:    credname.AccessToken,
-		DocsURL: sdk.URL("https://developer.crowdin.com/configuration-file/#api-credentials-from-environment-variables"),
+		Name:          credname.AccessToken,
+		DocsURL:       sdk.URL("https://developer.crowdin.com/configuration-file/#api-credentials-from-environment-variables"),
+		ManagementURL: sdk.URL("https://crowdin.com/settings#api-key"),
 		Fields: []schema.CredentialField{
 			{
 				Name:                fieldname.Token,
 				MarkdownDescription: "Token used to authenticate to Crowdin.",
 				Secret:              true,
 				Composition: &schema.ValueComposition{
-					Length: 80,
 					Charset: schema.Charset{
 						Lowercase: true,
 						Digits:    true,
