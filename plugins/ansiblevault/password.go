4428ba8273d4d7d2acd705e352589c76ebda511b
BOIS Pierre-Antoine <pro.pierreantoine.bois@gmail.com>
2023-08-01 12:08:17 +0200

    feat: create ansible vault plugin

---
+++ plugins/ansiblevault/password.go
@@ -0,0 +1,61 @@
+package ansiblevault
+
+import (
+	"context"
+	"github.com/1Password/shell-plugins/sdk"
+	"github.com/1Password/shell-plugins/sdk/importer"
+	"github.com/1Password/shell-plugins/sdk/provision"
+	"github.com/1Password/shell-plugins/sdk/schema"
+	"github.com/1Password/shell-plugins/sdk/schema/credname"
+	"github.com/1Password/shell-plugins/sdk/schema/fieldname"
+	"os"
+)
+
+func Password() schema.CredentialType {
+	return schema.CredentialType{
+		Name:          credname.Credential,
+		DocsURL:       sdk.URL("https://docs.ansible.com/ansible/latest/cli/ansible-vault.html"),
+		ManagementURL: nil,
+		Fields: []schema.CredentialField{
+			{
+				Name:                fieldname.Password,
+				MarkdownDescription: "Password to use when encrypting or decrypting.",
+				Secret:              true,
+			},
+		},
+		DefaultProvisioner: provision.TempFile(
+			passwordFile,
+			provision.Filename(".ansible-vault"),
+			provision.SetPathAsEnvVar("ANSIBLE_VAULT_PASSWORD_FILE"),
+		),
+		Importer: importer.TryAll(
+			TryPasswordFile(),
+		)}
+}
+
+func passwordFile(in sdk.ProvisionInput) ([]byte, error) {
+	contents := ""
+	if password, ok := in.ItemFields[fieldname.Password]; ok {
+		contents += password
+	}
+	return []byte(contents), nil
+}
+
+func TryPasswordFile() sdk.Importer {
+	file := os.Getenv("ANSIBLE_VAULT_PASSWORD_FILE")
+	if file == "" {
+		file = "~/.ansible-vault"
+	}
+	return importer.TryFile(file, func(ctx context.Context, contents importer.FileContents, in sdk.ImportInput, out *sdk.ImportAttempt) {
+		password := contents.ToString()
+		if password == "" {
+			return
+		}
+
+		out.AddCandidate(sdk.ImportCandidate{
+			Fields: map[sdk.FieldName]string{
+				fieldname.Password: password,
+			},
+		})
+	})
+}
