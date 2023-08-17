513bd6e2b32c49f5981980333351e1e82ae39f17
Arun <arun@arun.blog>
2023-08-16 23:26:01 -0400

    Don't skip auth for certain flags, rather use them in conjunction with 1Password-stored secrets to provision

--- plugins/mongodb/mongosh.go
+++ plugins/mongodb/mongosh.go
@@ -13,10 +13,6 @@ func MongoshCLI() schema.Executable {
 		Runs:    []string{"mongosh"},
 		DocsURL: sdk.URL("https://www.mongodb.com/docs/mongodb-shell/"),
 		NeedsAuth: needsauth.IfAll(
-			needsauth.NotWhenContainsArgs("--host"),     // skip 1Password auth if host is provided
-			needsauth.NotWhenContainsArgs("--port"),     // skip 1Password auth if port is provided
-			needsauth.NotWhenContainsArgs("--username"), // skip 1Password auth if username is provided
-			needsauth.NotWhenContainsArgs("--password"), // skip 1Password auth if password is provided
 			needsauth.NotForHelpOrVersion(),
 		),
 		Uses: []schema.CredentialUsage{
--- plugins/mongodb/provisioner.go
+++ plugins/mongodb/provisioner.go
@@ -2,17 +2,11 @@ package mongodb
 
 import (
 	"context"
+	"strings"
 
 	"github.com/1Password/shell-plugins/sdk"
 	"github.com/1Password/shell-plugins/sdk/schema/fieldname"
 )
-
-var argsToProvision = []string{
-	"--host", fieldname.Host.String(),
-	"--port", fieldname.Port.String(),
-	"--username", fieldname.Username.String(),
-	"--password", fieldname.Password.String(),
-}
 
 type mongodbShellArgsProvisioner struct {
 }
@@ -22,26 +16,83 @@ func mongodbShellProvisioner() sdk.Provisioner {
 }
 
 func (p mongodbShellArgsProvisioner) Provision(ctx context.Context, in sdk.ProvisionInput, out *sdk.ProvisionOutput) {
-	fieldsToSkipIfConnectionStringExists := []sdk.FieldName{
-		fieldname.Host,
-		fieldname.Port,
+	listOfPossibleUserInputArguments := []string{
+		"--host",
+		"--port",
+		"-u", "--username",
+		"-p", "--password",
 	}
+
+	var (
+		commandLineContainsHostArgument     bool = false
+		commandLineContainsPortArgument     bool = false
+		commandLineContainsUsernameArgument bool = false
+		commandLineContainsPasswordArgument bool = false
+	)
+
 	connectionStringProvisioned := false
-	if fieldValue, ok := in.ItemFields[fieldname.ConnectionString]; ok {
-		out.AddArgs(fieldValue)
+
+	for _, arg := range out.CommandLine {
+		if strings.HasPrefix(arg, "mongodb+srv://") {
+			connectionStringProvisioned = true
+			break
+		}
+	}
+
+	if value, ok := in.ItemFields[fieldname.ConnectionString]; ok && !connectionStringProvisioned {
+		out.AddArgs(value)
 		connectionStringProvisioned = true
 	}
-	for i, arg := range argsToProvision {
-		if i%2 == 0 {
-			argName := arg
-			fieldName := sdk.FieldName(argsToProvision[i+1])
-			if connectionStringProvisioned && contains(fieldsToSkipIfConnectionStringExists, fieldName) {
-				continue
-			}
-			if fieldValue, ok := in.ItemFields[fieldName]; ok {
-				out.AddArgsAtIndex(1, argName, fieldValue)
+
+	for i, arg := range out.CommandLine {
+		for _, userArg := range listOfPossibleUserInputArguments {
+			if arg == userArg {
+				// Get the executable "mongosh", the matched argument we are looking for and its value
+				commandLine := []string{out.CommandLine[0], out.CommandLine[i], out.CommandLine[i+1]}
+				// Remove the matched argument and its value as they will be added to the beginning of the command line
+				out.CommandLine = append(out.CommandLine[:i], out.CommandLine[i+2:]...)
+				// Add the executable "mongosh", the matched argument and its value to the beginning of the command line
+				commandLine = append(commandLine, out.CommandLine[1:]...)
+				out.CommandLine = commandLine
+				// Controller to check if the user has already provided the argument
+				switch userArg {
+				case "--host":
+					commandLineContainsHostArgument = true
+				case "--port":
+					commandLineContainsPortArgument = true
+				case "-u", "--username":
+					commandLineContainsUsernameArgument = true
+				case "-p", "--password":
+					commandLineContainsPasswordArgument = true
+				default:
+					break
+				}
 			}
 		}
+	}
+
+	if value, ok := in.ItemFields[fieldname.Host]; ok && !commandLineContainsHostArgument && !connectionStringProvisioned {
+		commandLine := []string{out.CommandLine[0], "-h", value}
+		commandLine = append(commandLine, out.CommandLine[1:]...)
+		out.CommandLine = commandLine
+	}
+
+	if value, ok := in.ItemFields[fieldname.Port]; ok && !commandLineContainsPortArgument && !connectionStringProvisioned {
+		commandLine := []string{out.CommandLine[0], "-p", value}
+		commandLine = append(commandLine, out.CommandLine[1:]...)
+		out.CommandLine = commandLine
+	}
+
+	if value, ok := in.ItemFields[fieldname.Username]; ok && !commandLineContainsUsernameArgument {
+		commandLine := []string{out.CommandLine[0], "--username", value}
+		commandLine = append(commandLine, out.CommandLine[1:]...)
+		out.CommandLine = commandLine
+	}
+
+	if value, ok := in.ItemFields[fieldname.Password]; ok && !commandLineContainsPasswordArgument {
+		commandLine := []string{out.CommandLine[0], "--password", value}
+		commandLine = append(commandLine, out.CommandLine[1:]...)
+		out.CommandLine = commandLine
 	}
 }
 
@@ -51,14 +102,4 @@ func (p mongodbShellArgsProvisioner) Deprovision(ctx context.Context, in sdk.Dep
 
 func (p mongodbShellArgsProvisioner) Description() string {
 	return "Provision MongoDB Shell secrets as command-line arguments."
-}
-
-// Helper function to check if a field is in a list of fields
-func contains(fields []sdk.FieldName, field sdk.FieldName) bool {
-	for _, f := range fields {
-		if f == field {
-			return true
-		}
-	}
-	return false
 }
