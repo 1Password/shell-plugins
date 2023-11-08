d27a4a8befed49f05acf912a804de9277737ce9d
kusshi <51787281+kusshi94@users.noreply.github.com>
2023-11-08 20:09:04 +0900

    update API Key Test

--- plugins/shodan/api_key_test.go
+++ plugins/shodan/api_key_test.go
@@ -11,12 +11,12 @@ import (
 func TestAPIKeyProvisioner(t *testing.T) {
 	plugintest.TestProvisioner(t, APIKey().DefaultProvisioner, map[string]plugintest.ProvisionCase{
 		"default": {
-			ItemFields: map[sdk.FieldName]string{ // TODO: Check if this is correct
+			ItemFields: map[sdk.FieldName]string{
 				fieldname.APIKey: "ddXfzwQOIjTaaxGMzcxXYR6Q0EXAMPLE",
 			},
 			ExpectedOutput: sdk.ProvisionOutput{
-				Environment: map[string]string{
-					"SHODAN_API_KEY": "ddXfzwQOIjTaaxGMzcxXYR6Q0EXAMPLE",
+				Files: map[string]sdk.OutputFile{
+					"~/.config/shodan/api_key": {Contents: []byte("ddXfzwQOIjTaaxGMzcxXYR6Q0EXAMPLE")},
 				},
 			},
 		},
@@ -25,9 +25,9 @@ func TestAPIKeyProvisioner(t *testing.T) {
 
 func TestAPIKeyImporter(t *testing.T) {
 	plugintest.TestImporter(t, APIKey().Importer, map[string]plugintest.ImportCase{
-		"environment": {
-			Environment: map[string]string{ // TODO: Check if this is correct
-				"SHODAN_API_KEY": "ddXfzwQOIjTaaxGMzcxXYR6Q0EXAMPLE",
+		"config file": {
+			Files: map[string]string{
+				"~/.config/shodan/api_key": plugintest.LoadFixture(t, "api_key"),
 			},
 			ExpectedCandidates: []sdk.ImportCandidate{
 				{
@@ -35,20 +35,6 @@ func TestAPIKeyImporter(t *testing.T) {
 						fieldname.APIKey: "ddXfzwQOIjTaaxGMzcxXYR6Q0EXAMPLE",
 					},
 				},
-			},
-		},
-		// TODO: If you implemented a config file importer, add a test file example in shodan/test-fixtures
-		// and fill the necessary details in the test template below.
-		"config file": {
-			Files: map[string]string{
-				// "~/path/to/config.yml": plugintest.LoadFixture(t, "config.yml"),
-			},
-			ExpectedCandidates: []sdk.ImportCandidate{
-			// 	{
-			// 		Fields: map[sdk.FieldName]string{
-			// 			fieldname.Token: "ddXfzwQOIjTaaxGMzcxXYR6Q0EXAMPLE",
-			// 		},
-			// 	},
 			},
 		},
 	})
