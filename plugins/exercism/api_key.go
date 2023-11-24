005b2b198c78e609d6772021d892017164ad217b
D E Costa <d.ethan.costa@gmail.com>
2023-11-24 17:56:05 -0500

    Add Exercism plugin

---
+++ plugins/exercism/api_key.go
@@ -0,0 +1,94 @@
+package exercism
+
+import (
+	"context"
+	"encoding/json"
+
+	"github.com/1Password/shell-plugins/sdk"
+	"github.com/1Password/shell-plugins/sdk/importer"
+	"github.com/1Password/shell-plugins/sdk/provision"
+	"github.com/1Password/shell-plugins/sdk/schema"
+	"github.com/1Password/shell-plugins/sdk/schema/credname"
+	"github.com/1Password/shell-plugins/sdk/schema/fieldname"
+)
+
+func APIKey() schema.CredentialType {
+	return schema.CredentialType{
+		Name:          credname.APIKey,
+		DocsURL:       sdk.URL("https://exercism.org/docs/using/solving-exercises/working-locally"),
+		ManagementURL: sdk.URL("https://exercism.org/settings/api_cli"),
+		Fields: []schema.CredentialField{
+			{
+				Name: fieldname.URL,
+				MarkdownDescription: `The URL of the Exercism API.`,
+				Secret: false,
+			},
+			{
+				Name:                fieldname.APIKey,
+				MarkdownDescription: "API Key used to authenticate to Exercism.",
+				Secret:              true,
+				Composition: &schema.ValueComposition{
+					Length: 37,
+					Charset: schema.Charset{
+						Lowercase: true,
+						Digits:    true,
+					},
+				},
+			},
+			{
+				Name: sdk.FieldName("Directory"),
+				MarkdownDescription: `The path to the workspace directory where the exercises are stored.`,
+				Secret: false,
+			},
+		},
+		DefaultProvisioner: provision.TempFile(
+			tempFileConfig,
+			provision.AtFixedPath("~/.config/exercism/user.json"),
+		),
+		Importer: importer.TryAll(
+			TryExercismConfigFile(),
+		)}
+}
+
+func TryExercismConfigFile() sdk.Importer {
+	return importer.TryFile("~/.config/exercism/user.json", func(ctx context.Context, contents importer.FileContents, in sdk.ImportInput, out *sdk.ImportAttempt) {
+		var config Config
+		if err := contents.ToJSON(&config); err != nil {
+			out.AddError(err)
+			return
+		}
+
+		if config.URL == "" || config.APIKey == "" || config.Workspace == "" {
+			return
+		}
+
+		out.AddCandidate(sdk.ImportCandidate{
+			Fields: map[sdk.FieldName]string{
+				fieldname.APIKey: config.APIKey,
+				fieldname.URL: config.URL,
+				sdk.FieldName("Directory"): config.Workspace,
+			},
+		})
+	})
+}
+
+type Config struct {
+	URL string `json:"apibaseurl"`
+	APIKey string `json:"token"`
+	Workspace string `json:"workspace"`
+}
+
+func tempFileConfig(in sdk.ProvisionInput) ([]byte, error) {
+	config := Config{
+		URL: in.ItemFields[fieldname.URL],
+		APIKey: in.ItemFields[fieldname.APIKey],
+		Workspace: in.ItemFields[sdk.FieldName("Directory")],
+	}
+
+	contents, err := json.Marshal(&config)
+	if err != nil {
+		return nil, err
+	}
+
+	return []byte(contents), nil
+}
---
+++ plugins/exercism/api_key_test.go
@@ -0,0 +1,48 @@
+package exercism
+
+import (
+	"strings"
+	"testing"
+	
+	"github.com/1Password/shell-plugins/sdk"
+	"github.com/1Password/shell-plugins/sdk/plugintest"
+	"github.com/1Password/shell-plugins/sdk/schema/fieldname"
+)
+	
+func TestAPIKeyProvisioner(t *testing.T) {
+	plugintest.TestProvisioner(t, APIKey().DefaultProvisioner, map[string]plugintest.ProvisionCase{
+		"default": {
+			ItemFields: map[sdk.FieldName]string{ 
+				fieldname.URL: "https://api.exercism.io/v1",
+				fieldname.APIKey: "v1o2p80wuf2qhnurrvf8rigro6sp38example",
+				sdk.FieldName("Directory"): "/Users/username/exercism",
+			},
+			ExpectedOutput: sdk.ProvisionOutput{
+				Files: map[string]sdk.OutputFile{
+					"~/.config/exercism/user.json": {
+						Contents: []byte(strings.Join(strings.Fields(plugintest.LoadFixture(t, "user.json")), "")),
+					},
+				},
+			},
+		},
+	})
+}
+
+func TestAPIKeyImporter(t *testing.T) {
+	plugintest.TestImporter(t, APIKey().Importer, map[string]plugintest.ImportCase{
+		"config file": {
+			Files: map[string]string{
+				"~/.config/exercism/user.json": plugintest.LoadFixture(t, "user.json"),
+			},
+			ExpectedCandidates: []sdk.ImportCandidate{
+			 	{
+			 		Fields: map[sdk.FieldName]string{
+						fieldname.URL: "https://api.exercism.io/v1",
+			 			fieldname.APIKey: "v1o2p80wuf2qhnurrvf8rigro6sp38example",
+						sdk.FieldName("Directory"): "/Users/username/exercism",
+			 		},
+			 	},
+			},
+		},
+	})
+}
