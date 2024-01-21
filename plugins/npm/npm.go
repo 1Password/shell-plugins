bd53b79a60269fd4a34e0b5b44ae5fbe60d9182b
Alon Dahari <alon@uptechstudio.com>
2024-01-21 17:14:30 +0000

    Add needsauth specification
    Add a list of commands that don't require authentication. When running
    those commands with the cli, the plugin will not try to fetch the token
    from the vault.
    
    I opted for a negative list of commands over a positive one since the
    latter would end up being much longer, would need to be more complete,
    and would probably need more updates with time, with newer npm versions.
    
    <!-- ps-id: ec70313e-76dd-4aab-811d-196348b499ac -->

--- plugins/npm/npm.go
+++ plugins/npm/npm.go
@@ -2,6 +2,7 @@ package npm
 
 import (
 	"github.com/1Password/shell-plugins/sdk"
+	"github.com/1Password/shell-plugins/sdk/needsauth"
 	"github.com/1Password/shell-plugins/sdk/schema"
 	"github.com/1Password/shell-plugins/sdk/schema/credname"
 )
@@ -11,6 +12,30 @@ func NPMCLI() schema.Executable {
 		Name:    "NPM CLI",
 		Runs:    []string{"npm"},
 		DocsURL: sdk.URL("https://docs.npmjs.com/cli"),
+		NeedsAuth: needsauth.IfAll(
+			// not a complete list of commands that don't
+			// need auth, but probably the main ones
+			needsauth.NotForHelpOrVersion(),
+			needsauth.NotWhenContainsArgs("init"),
+			needsauth.NotWhenContainsArgs("?"),
+			needsauth.NotWhenContainsArgs("config"),
+			needsauth.NotWhenContainsArgs("help-search"),
+			needsauth.NotWhenContainsArgs("login"),
+			needsauth.NotWhenContainsArgs("logout"),
+			needsauth.NotWhenContainsArgs("prune"),
+			needsauth.NotWhenContainsArgs("shrinkwrap"),
+			needsauth.NotWhenContainsArgs("start"),
+			needsauth.NotWhenContainsArgs("run-script"),
+			needsauth.NotWhenContainsArgs("run"),
+			needsauth.NotWhenContainsArgs("rum"),
+			needsauth.NotWhenContainsArgs("urn"),
+			needsauth.NotWhenContainsArgs("uninstall"),
+			needsauth.NotWhenContainsArgs("unlink"),
+			needsauth.NotWhenContainsArgs("remove"),
+			needsauth.NotWhenContainsArgs("rm"),
+			needsauth.NotWhenContainsArgs("r"),
+			needsauth.NotWhenContainsArgs("un"),
+		),
 		Uses: []schema.CredentialUsage{
 			{
 				Name: credname.AccessToken,
