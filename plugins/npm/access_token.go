994299a64ac719d101cd012ea64c5fd4ddbf70fc
Alon Dahari <alon@uptechstudio.com>
2024-01-25 13:58:09 +0000

    Support different registries
    NPM packages can live in different registries, not just the official
    one. This commit adds support for them by setting the env var according
    to the right backend.
    
    To support that, I had to use a temp file and not the env var
    provisioner, since the key name would be different per registry.
    
    I also added support for different scopes along the way.
    
    I decided to separate the credentials instead of having a single line
    ithe 1password vault, to allow users to configure their secrets more
    easily.
    
    <!-- ps-id: 7ec3e696-9c29-4354-bba5-ae4206bc871f -->

--- plugins/npm/access_token.go
+++ plugins/npm/access_token.go
@@ -5,6 +5,8 @@ import (
 	"os"
 	"strings"
 
+	"path/filepath"
+
 	"github.com/1Password/shell-plugins/sdk"
 	"github.com/1Password/shell-plugins/sdk/importer"
 	"github.com/1Password/shell-plugins/sdk/provision"
@@ -12,7 +14,6 @@ import (
 	"github.com/1Password/shell-plugins/sdk/schema/credname"
 	"github.com/1Password/shell-plugins/sdk/schema/fieldname"
 	"gopkg.in/ini.v1"
-	"path/filepath"
 )
 
 func AccessToken() schema.CredentialType {
@@ -35,8 +36,23 @@ func AccessToken() schema.CredentialType {
 					},
 				},
 			},
+			{
+				Name:                fieldname.Organization,
+				MarkdownDescription: "The organization the access token is scoped for.",
+				Optional:            true,
+			},
+			{
+				Name:                fieldname.Host,
+				MarkdownDescription: "The registry host for the npm packages.",
+				Optional:            true,
+			},
 		},
-		DefaultProvisioner: provision.EnvVars(defaultEnvVarMapping),
+		DefaultProvisioner: provision.TempFile(configFile,
+			provision.Filename(".npmrc"),
+			provision.AddArgs(
+				"--userconfig", "{{ .Path }}",
+			),
+		),
 		Importer: importer.TryAll(
 			TryNPMConfigFile(""),
 			TryGlobalNPMConfigFile("NPM_CONFIG_USERCONFIG", "~"),
@@ -44,8 +60,23 @@ func AccessToken() schema.CredentialType {
 	}
 }
 
-var defaultEnvVarMapping = map[string]sdk.FieldName{
-	"NPM_CONFIG_//registry.npmjs.org/:_authToken": fieldname.Token,
+func configFile(in sdk.ProvisionInput) ([]byte, error) {
+	contents := ""
+
+	if org, ok := in.ItemFields[fieldname.Organization]; ok && org != "" {
+		contents += "@" + strings.Trim(org, "@") + ":"
+	}
+	if host, ok := in.ItemFields[fieldname.Host]; ok && host != "" {
+		contents += "//" + strings.Trim(host, "/") + "/:"
+	}
+
+	contents += "_authToken="
+
+	if token, ok := in.ItemFields[fieldname.Token]; ok {
+		contents += token
+	}
+
+	return []byte(contents), nil
 }
 
 func TryGlobalNPMConfigFile(env string, defaultPath string) sdk.Importer {
@@ -74,10 +105,27 @@ func TryNPMConfigFile(path string) sdk.Importer {
 		for _, key := range section.Keys() {
 			if strings.Contains(key.Name(), "_authToken") {
 
+				keyParts := strings.Split(key.Name(), ":")
+
+				registry := ""
+				scope := ""
+				hint := ""
+				if len(keyParts) == 2 {
+					registry = strings.Trim(keyParts[0], "/")
+					hint = registry
+				} else if len(keyParts) == 3 {
+					registry = strings.Trim(keyParts[1], "/")
+					scope = strings.Trim(keyParts[0], "@")
+					hint = "@" + scope + ":" + registry
+				}
+
 				out.AddCandidate(sdk.ImportCandidate{
 					Fields: map[sdk.FieldName]string{
-						fieldname.Token: key.Value(),
+						fieldname.Token:        key.Value(),
+						fieldname.Host:         registry,
+						fieldname.Organization: scope,
 					},
+					NameHint: importer.SanitizeNameHint(hint),
 				})
 			}
 		}
