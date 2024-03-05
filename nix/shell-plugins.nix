2d9bb29cb2fd294c23323963097ae013abece902
Mat Jones <mat@mjones.network>
2024-03-05 07:32:15 -0500

    chore: Refactor to make it easier to validate

--- nix/shell-plugins.nix
+++ nix/shell-plugins.nix
@@ -22,6 +22,15 @@ in {
   };
 
   config = let
+    # executable names as strings, e.g. `pkgs.gh` => `"gh"`, `pkgs.awscli2` => `"aws"`
+    pkg-exe-names = map (package:
+      # NOTE: SAFETY: This is okay because the `packages` list is also referred
+      # to below as `home.packages = packages;` or `environment.systemPackages = packages;`
+      # depending on if it's using `home-manager` or not; this means that Nix can still
+      # compute the dependency tree, even though we're discarding string context here,
+      # since the packages are still referred to below without discarding string context.
+      builtins.unsafeDiscardStringContext (baseNameOf (getExe package)))
+      cfg.plugins;
     # Explanation:
     # Map over `cfg.plugins` (the value of the `plugins` option provided by the user)
     # and for each package specified, get the executable name, then create a shell alias
@@ -31,14 +40,7 @@ in {
     aliases = listToAttrs (map (package: {
       name = package;
       value = "op plugin run -- ${package}";
-    }) (map (package:
-      # NOTE: SAFETY: This is okay because the `packages` list is also referred
-      # to below as `home.packages = packages;` or `environment.systemPackages = packages;`
-      # depending on if it's using `home-manager` or not; this means that Nix can still
-      # compute the dependency tree, even though we're discarding string context here,
-      # since the packages are still referred to below without discarding string context.
-      builtins.unsafeDiscardStringContext (baseNameOf (getExe package)))
-      cfg.plugins));
+    }) pkg-exe-names);
     packages = [ pkgs._1password ] ++ cfg.plugins;
   in mkIf cfg.enable (mkMerge [
     ({
