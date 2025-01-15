381f42f5f12a8b6c13e69e7435bd726db1aed753
James Neill <git@jamesneill.codes>
2025-01-15 22:58:49 +0100

    feat(plugins/age): add a custom key pair provisioner
    - supports providing different handlers for different operation types (i.e. decrypt, encrypt)
    - in the future operation types could be extended depending potentially on argument combinations to allow better handling of edge case scenarios
    
    Signed-off-by: James Neill <git@jamesneill.codes>

--- plugins/age/errors.go
+++ plugins/age/errors.go
@@ -4,4 +4,5 @@ import "fmt"
 
 var (
 	ErrConflictingIdentityFlag = fmt.Errorf("conflict: the -i/--identity flag is automatically added by this plugin. Remove it from your command to continue")
+	ErrUnknownCommand          = fmt.Errorf("unknown command")
 )
---
+++ plugins/age/provisioner.go
@@ -0,0 +1,57 @@
+package age
+
+import (
+	"context"
+
+	"github.com/1Password/shell-plugins/sdk"
+	"github.com/1Password/shell-plugins/sdk/provision"
+)
+
+// KeyFiles holds the private and public key material as file contents.
+type KeyFiles struct {
+	private provision.ItemToFileContents
+	public  provision.ItemToFileContents
+}
+
+// KeyPairProvisioner handles the provisioning and deprovisioning of key files for the age command.
+type KeyPairProvisioner struct {
+	keys        KeyFiles
+	fileOptions []provision.FileOption
+}
+
+// Provision sets up the necessary key file for the `age` command based on the operation mode (Encrypt or Decrypt).
+// It determines the mode from the command line arguments and prepares the corresponding key file.
+func (p KeyPairProvisioner) Provision(ctx context.Context, in sdk.ProvisionInput, out *sdk.ProvisionOutput) {
+	operationHandlers := map[Operation]OperationHandler{}
+
+	mode := detectOperation(out.CommandLine)
+	handler, ok := operationHandlers[mode]
+	if !ok {
+		out.AddError(ErrUnknownCommand)
+		return
+	}
+
+	keyFileMaterialiser, args, filename := handler(p.keys, out)
+
+	p.fileOptions = append(p.fileOptions, provision.Filename(filename), provision.PrependArgs(args...))
+
+	fileProvisioner := provision.TempFile(keyFileMaterialiser, p.fileOptions...)
+	fileProvisioner.Provision(ctx, in, out)
+}
+
+// Deprovision performs cleanup after the process completes.
+// In this implementation, no cleanup is required as temporary files are automatically removed.
+func (p KeyPairProvisioner) Deprovision(ctx context.Context, in sdk.DeprovisionInput, out *sdk.DeprovisionOutput) {
+}
+
+func (p KeyPairProvisioner) Description() string {
+	return "Provision temporary file with public & private key pair & pass to age command"
+}
+
+// TempFile creates a new KeyPairProvisioner for handling temporary files for the age command.
+func TempFile(keys KeyFiles, opts ...provision.FileOption) sdk.Provisioner {
+	return KeyPairProvisioner{
+		keys:        keys,
+		fileOptions: opts,
+	}
+}
