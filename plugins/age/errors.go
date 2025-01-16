aaebe6bb488c29ac22446534c35de2c52b5c4f34
James Neill <contact@jamesneill.co.uk>
2025-01-16 13:05:00 +0100

    refactor(plugins/age): remove old provisioner implementation

--- plugins/age/errors.go
+++
@@ -1,8 +0,0 @@
-package age
-
-import "fmt"
-
-var (
-	ErrConflictingIdentityFlag = fmt.Errorf("conflict: the -i/--identity flag is automatically added by this plugin. Remove it from your command to continue")
-	ErrUnknownCommand          = fmt.Errorf("unknown command")
-)
