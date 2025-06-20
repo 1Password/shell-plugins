00e7531ef0bb96a5d680be4d251dff3ce53769ff
Michael Sterling <1461273+sterlinm@users.noreply.github.com>
2025-06-20 00:23:49 -0700

    - update management url - length is not fixed so remove - token can include . and _

--- plugins/motherduck/access_token.go
+++ plugins/motherduck/access_token.go
@@ -13,18 +13,18 @@ func AccessToken() schema.CredentialType {
 	return schema.CredentialType{
 		Name:          credname.AccessToken,
 		DocsURL:       sdk.URL("https://motherduck.com/docs/key-tasks/authenticating-and-connecting-to-motherduck/authenticating-to-motherduck/#authentication-using-an-access-token"),
-		ManagementURL: sdk.URL("https://app.motherduck.com/settings/general"),
+		ManagementURL: sdk.URL("https://app.motherduck.com/settings/tokens"),
 		Fields: []schema.CredentialField{
 			{
 				Name:                fieldname.Token,
 				MarkdownDescription: "Token used to authenticate to MotherDuck.",
 				Secret:              true,
 				Composition: &schema.ValueComposition{
-					Length: 405,
 					Charset: schema.Charset{
 						Uppercase: true,
 						Lowercase: true,
 						Digits:    true,
+						Specific:  []rune{'.', '_'},
 					},
 				},
 			},
