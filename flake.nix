6e92afdde1fd9269a6af7c9743744cd8a8399029
Luis Quiñones <lpaandres2020@gmail.com>
2025-08-25 17:17:28 -0500

    refactor(flake): replace flake-utils with nix-systems

--- flake.nix
+++ flake.nix
@@ -1,18 +1,24 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    flake-utils = { url = "github:numtide/flake-utils"; };
+    systems.url = "github:nix-systems/default";
   };
-  outputs = { nixpkgs, flake-utils, ... }:
-    (flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system};
-      in {
-        devShells.default = pkgs.mkShell {
+  outputs = { nixpkgs, systems, ... }:
+    let
+      forAllSystems = function:
+        nixpkgs.lib.genAttrs (import systems) (
+          system: function nixpkgs.legacyPackages.${system}
+        );
+    in
+    {
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
           name = "Shell with Go toolchain";
           packages = with pkgs; [ go gopls ];
         };
-      })) // {
-        nixosModules.default = import ./nix/nixos.nix;
-        hmModules.default = import ./nix/home-manager.nix;
-      };
+      });
+
+      nixosModules.default = import ./nix/nixos.nix;
+      hmModules.default = import ./nix/home-manager.nix;
+    };
 }
