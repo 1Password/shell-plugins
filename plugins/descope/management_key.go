5262f6a9c26bd0e480c94470c0743c70f39526d8
Scott Lougheed <scott.lougheed@agilebits.com>
2026-01-02 08:38:49 -0800

    removing unused code

--- plugins/descope/management_key.go
+++ plugins/descope/management_key.go
@@ -1,8 +1,6 @@
 package descope
 
 import (
-	"context"
-
 	"github.com/1Password/shell-plugins/sdk"
 	"github.com/1Password/shell-plugins/sdk/importer"
 	"github.com/1Password/shell-plugins/sdk/provision"
@@ -44,7 +42,6 @@ func ManagementKey() schema.CredentialType {
 		DefaultProvisioner: provision.EnvVars(defaultEnvVarMapping),
 		Importer: importer.TryAll(
 			importer.TryEnvVarPair(defaultEnvVarMapping),
-			TryDescopeConfigFile(),
 		),
 	}
 }
@@ -53,30 +50,3 @@ var defaultEnvVarMapping = map[string]sdk.FieldName{
 	"DESCOPE_PROJECT_ID":     fieldname.ProjectID,
 	"DESCOPE_MANAGEMENT_KEY": fieldname.ManagementKey,
 }
-
-// TODO: Check if the platform stores the Management Key in a local config file, and if so,
-// implement the function below to add support for importing it.
-func TryDescopeConfigFile() sdk.Importer {
-	return importer.TryFile("~/path/to/config/file.yml", func(ctx context.Context, contents importer.FileContents, in sdk.ImportInput, out *sdk.ImportAttempt) {
-		// var config Config
-		// if err := contents.ToYAML(&config); err != nil {
-		// 	out.AddError(err)
-		// 	return
-		// }
-
-		// if config.Key == "" {
-		// 	return
-		// }
-
-		// out.AddCandidate(sdk.ImportCandidate{
-		// 	Fields: map[sdk.FieldName]string{
-		// 		fieldname.Key: config.Key,
-		// 	},
-		// })
-	})
-}
-
-// TODO: Implement the config file schema
-// type Config struct {
-//	Key string
-// }
