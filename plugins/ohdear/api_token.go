6c7a2885910dd23cf185c471e8fcb81f1aa973d1
Owen Voke <development@voke.dev>
2026-02-25 08:19:41 +0000

    fix: resolve support for Oh Dear CLI 5.x

--- plugins/ohdear/api_token.go
+++ plugins/ohdear/api_token.go
@@ -1,6 +1,9 @@
 package ohdear
 
 import (
+	"context"
+	"encoding/json"
+
 	"github.com/1Password/shell-plugins/sdk"
 	"github.com/1Password/shell-plugins/sdk/importer"
 	"github.com/1Password/shell-plugins/sdk/provision"
@@ -12,7 +15,7 @@ import (
 func APIToken() schema.CredentialType {
 	return schema.CredentialType{
 		Name:          credname.APIToken,
-		DocsURL:       sdk.URL("https://ohdear.app/docs/integrations/the-oh-dear-api#get-your-api-token"),
+		DocsURL:       sdk.URL("https://ohdear.app/docs/api/introduction#get-your-api-token"),
 		ManagementURL: sdk.URL("https://ohdear.app/user/api-tokens"),
 		Fields: []schema.CredentialField{
 			{
@@ -29,12 +32,53 @@ func APIToken() schema.CredentialType {
 				},
 			},
 		},
-		DefaultProvisioner: provision.EnvVars(defaultEnvVarMapping),
+		DefaultProvisioner: provision.TempFile(configFile,
+			provision.Filename("config.json"),
+			provision.SetPathAsEnvVar("OHDEAR_CONFIG_FILE_PATH"),
+		),
 		Importer: importer.TryAll(
 			importer.TryEnvVarPair(defaultEnvVarMapping),
+			TryOhdearConfigFile(),
 		)}
 }
 
 var defaultEnvVarMapping = map[string]sdk.FieldName{
 	"OHDEAR_API_TOKEN": fieldname.Token,
+}
+
+func configFile(in sdk.ProvisionInput) ([]byte, error) {
+	config := Config{
+		Token: in.ItemFields[fieldname.Token],
+	}
+
+	contents, err := json.Marshal(config)
+	if err != nil {
+		return nil, err
+	}
+
+	return []byte(contents), nil
+}
+
+func TryOhdearConfigFile() sdk.Importer {
+	return importer.TryFile("~/.ohdear/config.json", func(ctx context.Context, contents importer.FileContents, in sdk.ImportInput, out *sdk.ImportAttempt) {
+		var config Config
+		if err := contents.ToJSON(&config); err != nil {
+			out.AddError(err)
+			return
+		}
+
+		if config.Token == "" {
+			return
+		}
+
+		out.AddCandidate(sdk.ImportCandidate{
+			Fields: map[sdk.FieldName]string{
+				fieldname.Token: config.Token,
+			},
+		})
+	})
+}
+
+type Config struct {
+	Token string `json:"token"`
 }
