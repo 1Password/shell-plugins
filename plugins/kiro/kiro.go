ae128e2823af1ded441963185483766af4438127
Eddú Meléndez <eddu.melendez@gmail.com>
2026-05-28 23:32:52 -0600

    Add Kiro shell plugin
    Signed-off-by: Eddú Meléndez <eddu.melendez@gmail.com>

---
+++ plugins/kiro/kiro.go
@@ -0,0 +1,22 @@
+package kiro
+
+import (
+	"github.com/1Password/shell-plugins/sdk"
+	"github.com/1Password/shell-plugins/sdk/needsauth"
+	"github.com/1Password/shell-plugins/sdk/schema"
+	"github.com/1Password/shell-plugins/sdk/schema/credname"
+)
+
+func KiroCLI() schema.Executable {
+	return schema.Executable{
+		Name:      "Kiro CLI",
+		Runs:      []string{"kiro-cli"},
+		DocsURL:   sdk.URL("https://kiro.dev/docs/cli/"),
+		NeedsAuth: needsauth.NotForHelpOrVersion(),
+		Uses: []schema.CredentialUsage{
+			{
+				Name: credname.APIKey,
+			},
+		},
+	}
+}
