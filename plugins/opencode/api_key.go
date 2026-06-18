1d31eb34c6d2feb9edff744adaed98fbcb4e20f9
Eddú Meléndez <eddu.melendez@gmail.com>
2026-06-17 21:57:29 -0600

    Add OpenCode shell plugin

---
+++ plugins/opencode/api_key.go
@@ -0,0 +1,31 @@
+package opencode
+
+import (
+	"github.com/1Password/shell-plugins/sdk"
+	"github.com/1Password/shell-plugins/sdk/importer"
+	"github.com/1Password/shell-plugins/sdk/provision"
+	"github.com/1Password/shell-plugins/sdk/schema"
+	"github.com/1Password/shell-plugins/sdk/schema/credname"
+	"github.com/1Password/shell-plugins/sdk/schema/fieldname"
+)
+
+func APIKey() schema.CredentialType {
+	return schema.CredentialType{
+		Name:          credname.APIKey,
+		DocsURL:       sdk.URL("https://opencode.ai/docs/"),
+		ManagementURL: sdk.URL("https://opencode.ai/auth"),
+		Fields: []schema.CredentialField{
+			{
+				Name:                fieldname.APIKey,
+				MarkdownDescription: "API Key used to authenticate to opencode.",
+				Secret:              true,
+			},
+		},
+		DefaultProvisioner: provision.EnvVars(defaultEnvVarMapping),
+		Importer:           importer.TryEnvVarPair(defaultEnvVarMapping),
+	}
+}
+
+var defaultEnvVarMapping = map[string]sdk.FieldName{
+	"OPENCODE_API_KEY": fieldname.APIKey,
+}
---
+++ plugins/opencode/opencode.go
@@ -0,0 +1,25 @@
+package opencode
+
+import (
+	"github.com/1Password/shell-plugins/sdk"
+	"github.com/1Password/shell-plugins/sdk/needsauth"
+	"github.com/1Password/shell-plugins/sdk/schema"
+	"github.com/1Password/shell-plugins/sdk/schema/credname"
+)
+
+func OpencodeCLI() schema.Executable {
+	return schema.Executable{
+		Name:    "opencode CLI",
+		Runs:    []string{"opencode"},
+		DocsURL: sdk.URL("https://opencode.ai/docs/"),
+		NeedsAuth: needsauth.IfAll(
+			needsauth.NotForHelpOrVersion(),
+			needsauth.NotWithoutArgs(),
+		),
+		Uses: []schema.CredentialUsage{
+			{
+				Name: credname.APIKey,
+			},
+		},
+	}
+}
