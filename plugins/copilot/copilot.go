9c21fd33a83cdf60c1d471ab2f42b64a2fb496a2
Eddú Meléndez <eddu.melendez@gmail.com>
2026-06-19 16:59:13 -0600

    Fix comments

--- plugins/copilot/copilot.go
+++ plugins/copilot/copilot.go
@@ -9,10 +9,16 @@ import (
 
 func CopilotCLI() schema.Executable {
 	return schema.Executable{
-		Name:      "GitHub Copilot CLI",
-		Runs:      []string{"copilot"},
-		DocsURL:   sdk.URL("https://docs.github.com/en/copilot/reference/copilot-cli-reference/cli-command-reference"),
-		NeedsAuth: needsauth.NotForHelpOrVersion(),
+		Name:    "GitHub Copilot CLI",
+		Runs:    []string{"copilot"},
+		DocsURL: sdk.URL("https://docs.github.com/en/copilot/reference/copilot-cli-reference/cli-command-reference"),
+		NeedsAuth: needsauth.IfAll(
+			needsauth.NotForHelpOrVersion(),
+			needsauth.NotWithoutArgs(),
+			needsauth.NotForExactArgs("login"),
+			needsauth.NotForExactArgs("version"),
+			needsauth.NotWhenContainsArgs("completion"),
+		),
 		Uses: []schema.CredentialUsage{
 			{
 				Name: credname.AuthToken,
